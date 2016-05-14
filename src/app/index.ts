324f533140fc94d7b190b887b399e4084d5a3a01
angular-cli <angular-cli@angular.io>
2016-05-14 13:06:47 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export {environment} from './environment';
+export {ScheduleAppComponent} from './schedule.component';
---
+++ src/app/schedule.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'schedule-app',
+  templateUrl: 'schedule.component.html',
+  styleUrls: ['schedule.component.css']
+})
+export class ScheduleAppComponent {
+  title = 'schedule works!';
+}
